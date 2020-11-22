45882e43e7c48f9683464cbab359d3d7355c0c9c
Chivy Lim <chivy@web-essentials.asia>
2020-11-22 10:53:53 +0700

    [TASK] Update favicon
    refs TRA-37

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import {
   FilteringState,
-  IntegratedFiltering,
   SearchState,
   PagingState,
-  IntegratedPaging
+  CustomPaging
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
@@ -23,19 +22,16 @@ import Toolbar from 'components/Table/Toolbar';
 import SearchInput from 'components/Table/SearchPanel/Input';
 import FilterToggle from 'components/Table/FilterToggle';
 import ToggleButton from 'components/Table/ColumnChooser/ToggleButton';
-// import PagingPanelContainer from 'components/Table/PagingPanel/Container';
 
 import '@icon/open-iconic/open-iconic.css';
 import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
-const CustomTable = ({ columns, rows }) => {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [pageSize, setPageSize] = useState(10);
+const CustomTable = ({ rows, columns, columnExtensions, pageSize, setPageSize, currentPage, setCurrentPage, totalCount, setSearchValue }) => {
   const [showFilter, setShowFilter] = useState(false);
 
   const pageSizes = [10, 20, 30, 40, 50];
   const rightColumns = ['action'];
-  const tableColumnExtensions = [{ columnName: 'action', align: 'right', width: 120 }];
+  const tableColumnExtensions = [...columnExtensions, { columnName: 'action', align: 'right', width: 120 }];
   const tableColumnVisibilityColumnExtensions = [{ columnName: 'action', togglingEnabled: false }];
   const filteringStateColumnExtensions = [{ columnName: 'action', filteringEnabled: false }];
   const FilterRow = (props) => <Table.Row className="filter" {...props} />;
@@ -49,16 +45,17 @@ const CustomTable = ({ columns, rows }) => {
     <Grid
       rows={rows}
       columns={columns}>
-      <SearchState />
+      <SearchState onValueChange={setSearchValue} />
       <FilteringState defaultFilters={[]} columnExtensions={filteringStateColumnExtensions} />
-      <IntegratedFiltering />
       <PagingState
         currentPage={currentPage}
         onCurrentPageChange={setCurrentPage}
         pageSize={pageSize}
         onPageSizeChange={setPageSize}
       />
-      <IntegratedPaging />
+      <CustomPaging
+        totalCount={totalCount}
+      />
 
       <Table columnExtensions={tableColumnExtensions} />
       <TableHeaderRow />
@@ -70,14 +67,25 @@ const CustomTable = ({ columns, rows }) => {
       <SearchPanel inputComponent={SearchInput} />
       <FilterToggle onToggle={toggleFilter} showFilter={showFilter} />
       <ColumnChooser toggleButtonComponent={ToggleButton} />
-
       <PagingPanel pageSizes={pageSizes} />
     </Grid>
   );
 };
 
 CustomTable.propTypes = {
+  rows: PropTypes.array,
   columns: PropTypes.array,
-  rows: PropTypes.array
+  columnExtensions: PropTypes.array,
+  pageSize: PropTypes.number,
+  setPageSize: PropTypes.func,
+  currentPage: PropTypes.number,
+  setCurrentPage: PropTypes.func,
+  totalCount: PropTypes.number,
+  setSearchValue: PropTypes.func
 };
+
+CustomTable.defaultProps = {
+  columnExtensions: []
+};
+
 export default CustomTable;
