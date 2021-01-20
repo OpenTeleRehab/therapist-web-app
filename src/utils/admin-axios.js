814da18557e1e2319e897c521fe54888ee39c226
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2021-01-20 11:09:34 +0700

    [IMP] Add language filter for exercise creation
    refs TRA-256

--- src/utils/admin-axios.js
+++ src/utils/admin-axios.js
@@ -5,12 +5,4 @@ const adminaxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_ADMIN_API_BASE_URL
 });
 
-adminaxiosInstance.interceptors.request.use((config) => {
-  const { lang } = window;
-  if (lang) {
-    config.params = { ...config.params, lang };
-  }
-  return config;
-});
-
 export default adminaxiosInstance;
