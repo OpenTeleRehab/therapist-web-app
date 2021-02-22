27c895f05c4d159fb12f2af7d7619f7c1ee62b13
Lina <lina@web-essentials.co>
2021-02-22 16:44:45 +0700

    [TASK] View questionnaire detail pop-up
    refs TRA-115

--- src/views/TreatmentPlan/TabContents/questionnaireTab.js
+++ src/views/TreatmentPlan/TabContents/questionnaireTab.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import CustomTable from '../../../components/Table';
 import moment from 'moment/moment';
 import settings from '../../../settings';
+import { ViewAction } from 'components/ActionIcons';
+import ViewQuestion from './viewQuestion';
 
 const QuestionnaireTab = ({ activities }) => {
   const localize = useSelector((state) => state.localize);
@@ -12,6 +14,8 @@ const QuestionnaireTab = ({ activities }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const [questionnaires, setQuestionnaires] = useState([]);
+  const [show, setShow] = useState(false);
+  const [questionnaire, setQuestionnaire] = useState([]);
 
   useEffect(() => {
     if (activities.length) {
@@ -23,15 +27,26 @@ const QuestionnaireTab = ({ activities }) => {
   const columns = [
     { name: 'submitted_date', title: translate('questionnaire.submitted_date') },
     { name: 'title', title: translate('questionnaire.title') + '/' + translate('questionnaire.description') },
-    { name: 'number_of_question', title: translate('questionnaire.number_of_question') }
+    { name: 'number_of_question', title: translate('questionnaire.number_of_question') },
+    { name: 'action', title: translate('common.action') }
   ];
 
   const defaultSoringColumns = [
     { columnName: 'submitted_date', direction: 'desc' }
   ];
 
+  const handleView = (questionnaire) => {
+    setQuestionnaire(questionnaire);
+    setShow(true);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
   return (
     <div className="mt-3">
+      {show && <ViewQuestion show={show} handleClose={handleClose} questionnaire={questionnaire}/>}
       <CustomTable
         pageSize={pageSize}
         setPageSize={setPageSize}
@@ -42,6 +57,9 @@ const QuestionnaireTab = ({ activities }) => {
         defaultSoringColumns={defaultSoringColumns}
         remotePaging={false}
         rows={questionnaires.map(questionnaire => {
+          const action = (
+            <ViewAction onClick={() => handleView(questionnaire)}/>
+          );
           return {
             submitted_date: questionnaire.submitted_date ? moment(questionnaire.submitted_date).format(settings.date_format) : '',
             title: <span
@@ -50,9 +68,11 @@ const QuestionnaireTab = ({ activities }) => {
                 __html: `<strong class="title">${questionnaire.title}</strong><div class="description">${questionnaire.description}</div>`
               }}
             />,
-            number_of_question: questionnaire.questions.length
+            number_of_question: questionnaire.questions.length,
+            action
           };
-        })}
+        })
+        }
       />
     </div>
   );
---
+++ src/views/TreatmentPlan/TabContents/viewQuestion.js
@@ -0,0 +1,138 @@
+import React, { useContext } from 'react';
+import Dialog from 'components/Dialog';
+import { useSelector } from 'react-redux';
+import { getTranslate } from 'react-localize-redux';
+import PropTypes from 'prop-types';
+import 'react-phone-input-2/lib/style.css';
+import { Accordion, AccordionContext, Card, Form } from 'react-bootstrap';
+import _ from 'lodash';
+import { BsChevronDown, BsChevronRight } from 'react-icons/bs';
+
+const ViewQuestion = ({ show, handleClose, questionnaire }) => {
+  const localize = useSelector((state) => state.localize);
+  const translate = getTranslate(localize);
+
+  const answerValue = (questionId) => {
+    const answer = questionnaire.answers.find(patientAnswer => patientAnswer.question_id === questionId);
+    return answer ? answer.answer : '';
+  };
+
+  return (
+    <Dialog
+      show={show}
+      size="lg"
+      scrollable={true}
+      title={questionnaire.title}
+      onCancel={handleClose}
+      cancelLabel={translate('common.close')}
+    >
+      <div className="d-flex flex-column mb-2">
+        <span className="font-weight-bold">{translate('questionnaire.description')}</span>
+        <span>{questionnaire.description}</span>
+      </div>
+      <div className="d-flex flex-column mb-3">
+        <span className="font-weight-bold">{translate('questionnaire.number_of_question')}</span>
+        <span>{questionnaire.questions.length}</span>
+      </div>
+      {questionnaire.questions.map((question, index) => (
+        <Accordion key={index}>
+          <Card className="mb-3 question-card">
+            <Accordion.Toggle as={Card.Header} eventKey={index + 1} className="card-header d-flex justify-content-between">
+              <h6>{translate('questionnaire.question_number', { number: index + 1 })}</h6>
+              <ContextAwareToggle eventKey={index + 1} />
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={index + 1}>
+              <Card.Body>
+                { question.file &&
+                  <img src={`${process.env.REACT_APP_ADMIN_API_BASE_URL}/file/${question.file.id}`} alt="..." className="img-thumbnail"/>
+                }
+                <div>
+                  {question.title}
+                </div>
+                <div>
+                  {
+                    question.type === 'checkbox' && (
+                      question.answers.map((answer, index) => (
+                        <div key={index}>
+                          <Form.Check
+                            inline label={answer.description}
+                            type='checkbox'
+                            checked={ questionnaire.answers.find(patientAnswer => _.includes(patientAnswer.answer, answer.id))}
+                            disabled
+                          />
+                        </div>
+                      ))
+                    )
+                  }
+                  {
+                    question.type === 'multiple' && (
+                      question.answers.map((answer, index) => (
+                        <div key={index}>
+                          <Form.Check
+                            inline label={answer.description}
+                            type='radio'
+                            checked={questionnaire.answers.find(patientAnswer => patientAnswer.answer === answer.id)}
+                            disabled
+                          />
+                        </div>
+                      ))
+                    )
+                  }
+                  {
+                    question.type === 'open-text' && (
+                      <div className="ml-1">
+                        <Form.Group controlId='formValue'>
+                          <Form.Control
+                            disabled
+                            as="textarea"
+                            value={answerValue(question.id)}
+                          />
+                        </Form.Group>
+                      </div>
+                    )
+                  }
+                  {
+                    question.type === 'open-number' && (
+                      <div className="ml-1">
+                        <Form.Group controlId='formValue'>
+                          <Form.Control
+                            disabled
+                            type="number"
+                            value={answerValue(question.id)}
+                          />
+                        </Form.Group>
+                      </div>
+                    )
+                  }
+                </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      ))
+      }
+    </Dialog>
+  );
+};
+
+ViewQuestion.propTypes = {
+  show: PropTypes.bool,
+  handleClose: PropTypes.func,
+  questionnaire: PropTypes.array
+};
+
+export default ViewQuestion;
+
+const ContextAwareToggle = ({ eventKey }) => {
+  const currentEventKey = useContext(AccordionContext);
+
+  if (currentEventKey === eventKey) {
+    return <BsChevronDown className="ml-auto" />;
+  }
+
+  return <BsChevronRight className="ml-auto" />;
+};
+
+ContextAwareToggle.propTypes = {
+  eventKey: PropTypes.string
+};
