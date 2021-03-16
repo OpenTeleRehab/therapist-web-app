3d3445d4f1feaf73e21a249b7df0fe659e60e5ca
Lina <lina@web-essentials.co>
2021-03-16 20:15:13 +0700

    [TASK] Implement material tab
    refs TRA-354

--- src/views/Library/EducationMaterial/index.js
+++ src/views/Library/EducationMaterial/index.js
@@ -1,11 +1,201 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withLocalize } from 'react-localize-redux';
+import {
+  Row,
+  Col,
+  Card,
+  Form,
+  Tooltip,
+  OverlayTrigger,
+  Button
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { BsSearch, BsX } from 'react-icons/bs';
 
+import Pagination from 'components/Pagination';
+import Spinner from 'react-bootstrap/Spinner';
+import { MdDescription } from 'react-icons/md';
+import { getEducationMaterials } from 'store/educationMaterial/actions';
+import ViewEducationMaterial from './viewEducationMaterial';
+
+let timer = null;
 const EducationMaterial = ({ translate }) => {
+  const dispatch = useDispatch();
+  const { loading, educationMaterials, filters } = useSelector(state => state.educationMaterial);
+  const [pageSize, setPageSize] = useState(8);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
+  const [formFields, setFormFields] = useState({
+    search_value: ''
+  });
+
+  const languages = useSelector(state => state.language.languages);
+  const [language, setLanguage] = useState('');
+  const { profile } = useSelector((state) => state.auth);
+  const [educationMaterial, setEducationMaterial] = useState([]);
+  const [viewEducationMaterial, setViewEducationMaterial] = useState(false);
+
+  useEffect(() => {
+    if (filters && filters.lang) {
+      setLanguage(filters.lang);
+    } else if (profile && profile.language_id) {
+      setLanguage(profile.language_id);
+    }
+  }, [filters, profile]);
+
+  useEffect(() => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      dispatch(getEducationMaterials({
+        filter: formFields,
+        page_size: pageSize,
+        lang: language,
+        page: currentPage
+      })).then(result => {
+        if (result) {
+          setTotalCount(result.total_count);
+        }
+      });
+    }, 500);
+  }, [language, formFields, currentPage, pageSize, dispatch]);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleClearSearch = () => {
+    setFormFields({ ...formFields, search_value: '' });
+  };
+
+  const handleLanguageChange = e => {
+    const { value } = e.target;
+    setLanguage(value);
+  };
+
+  const handleViewEducationMaterial = (educationMaterial) => {
+    setViewEducationMaterial(true);
+    setEducationMaterial(educationMaterial);
+  };
+
+  const handleViewEducationMaterialClose = () => {
+    setViewEducationMaterial(false);
+  };
+
   return (
     <>
-      This is educationMaterials
+      <Row>
+        <Col sm={5} md={4} lg={3}>
+          <Card bg="info">
+            <Card.Header>
+              <Form.Group className="search-box-with-icon">
+                <BsSearch className="search-icon" />
+                <Button
+                  variant="light"
+                  className="clear-btn"
+                  onClick={handleClearSearch}
+                >
+                  <BsX size={18} />
+                </Button>
+                <Form.Control
+                  name="search_value"
+                  value={formFields.search_value}
+                  onChange={handleChange}
+                  placeholder={translate('education_material.search')}
+                />
+              </Form.Group>
+            </Card.Header>
+            <Card.Body>
+              <Form.Group>
+                <Form.Label>{translate('common.category')}</Form.Label>
+                <Form.Control as="select" disabled>
+                  <option>{translate('common.category_item')}</option>
+                  <option>{translate('common.category_item')}</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>{translate('common.category')}</Form.Label>
+                <Form.Control as="select" disabled>
+                  <option>{translate('common.category_item')}</option>
+                  <option>{translate('common.category_item')}</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>{translate('common.category')}</Form.Label>
+                <Form.Control as="select" disabled>
+                  <option>{translate('common.category_item')}</option>
+                  <option>{translate('common.category_item')}</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>{translate('common.language')}</Form.Label>
+                <Form.Control as="select" value={language} onChange={handleLanguageChange}>
+                  {languages.map((language, index) => (
+                    <option key={index} value={language.id}>
+                      {language.name}
+                    </option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={7} md={8} lg={9}>
+          { educationMaterials.length === 0 && (
+            <div className="card h-100 d-flex justify-content-center align-items-center">
+              <big className="text-muted">{translate('common.no_data')}</big>
+            </div>
+          )}
+          { educationMaterials.length > 0 && (
+            <>
+              <Row>
+                { educationMaterials.map(material => (
+                  <Col key={material.id} md={6} lg={3}>
+                    <Card className="exercise-card material-card shadow-sm mb-4" onClick={() => handleViewEducationMaterial(material)}>
+                      <div className="card-img bg-light">
+                        <div className="w-100 h-100 px-2 py-4 text-white bg-primary text-center">
+                          <MdDescription size={80} />
+                          <p>{translate('activity.material').toUpperCase()}</p>
+                        </div>
+                      </div>
+                      <Card.Body className="d-flex flex-column justify-content-between">
+                        <Card.Title>
+                          {
+                            material.title.length <= 50
+                              ? <h5 className="card-title">{ material.title }</h5>
+                              : (
+                                <OverlayTrigger
+                                  overlay={<Tooltip id="button-tooltip-2">{ material.title }</Tooltip>}
+                                >
+                                  <h5 className="card-title">{ material.title }</h5>
+                                </OverlayTrigger>
+                              )
+                          }
+                        </Card.Title>
+                        <Card.Text>
+                          {translate(material.file.fileGroupType)}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+
+              <Pagination
+                totalCount={totalCount}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                pageSize={pageSize}
+                setPageSize={setPageSize}
+                pageSizes={[8, 16, 24, 32, 40]}
+              />
+            </>
+          )}
+          { loading && <Spinner className="loading-icon" animation="border" variant="primary" /> }
+          { viewEducationMaterial && <ViewEducationMaterial showView={viewEducationMaterial} handleViewClose={handleViewEducationMaterialClose} educationMaterial={educationMaterial} />}
+        </Col>
+      </Row>
     </>
   );
 };
---
+++ src/views/Library/EducationMaterial/viewEducationMaterial.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withLocalize } from 'react-localize-redux';
+import { Col, Form, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import Dialog from 'components/Dialog';
+import { getTranslate } from 'react-localize-redux/lib/index';
+
+const ViewEducationMaterial = ({ showView, handleViewClose, educationMaterial }) => {
+  const localize = useSelector((state) => state.localize);
+  const translate = getTranslate(localize);
+
+  return (
+    <Dialog
+      show={showView}
+      title={educationMaterial.title}
+      cancelLabel={translate('common.close')}
+      onCancel={handleViewClose}
+    >
+      <Form>
+        <Row>
+          <Col sm={12} xl={12}>
+            { educationMaterial &&
+                <div className = "exercise-media">
+                  { educationMaterial.file.fileType === 'audio/mpeg' &&
+                    <div className="img-thumbnail w-100 pt-2">
+                      <audio controls className="w-100">
+                        <source src={educationMaterial.file.url || `${process.env.REACT_APP_ADMIN_API_BASE_URL}/file/${educationMaterial.file.id}`} type="audio/ogg" />
+                      </audio>
+                    </div>
+                  }
+
+                  { (educationMaterial.file.fileType !== 'audio/mpeg' && educationMaterial.file.fileType !== 'video/mp4' && educationMaterial.file.fileType !== 'application/pdf') &&
+                    <img src={educationMaterial.file.url || `${process.env.REACT_APP_ADMIN_API_BASE_URL}/file/${educationMaterial.file.id}`} alt="..." className="w-100 img-thumbnail"/>
+                  }
+
+                  { educationMaterial.file.fileType === 'video/mp4' &&
+                    <video className="w-100 img-thumbnail" controls>
+                      <source src={educationMaterial.file.url || `${process.env.REACT_APP_ADMIN_API_BASE_URL}/file/${educationMaterial.file.id}`} type="video/mp4" />
+                    </video>
+                  }
+
+                  { educationMaterial.file.fileType === 'application/pdf' &&
+                    <object
+                      data={educationMaterial.file.url || `${process.env.REACT_APP_ADMIN_API_BASE_URL}/file/${educationMaterial.file.id}`}
+                      type="application/pdf"
+                      width="480"
+                      height="678"
+                    >
+                      <iframe
+                        src={educationMaterial.file.url || `${process.env.REACT_APP_ADMIN_API_BASE_URL}/file/${educationMaterial.file.id}`}
+                        width="480"
+                        height="678"
+                        title={educationMaterial.file.fileName}
+                      >
+                        <p>This browser does not support PDF!</p>
+                      </iframe>
+                    </object>
+                  }
+
+                  <Form.Text className="text-muted">
+                    {translate(educationMaterial.file.fileGroupType)}:
+                    <a
+                      href={`${process.env.REACT_APP_ADMIN_API_BASE_URL}/file/${educationMaterial.file.id}`}
+                      /* eslint-disable-next-line react/jsx-no-target-blank */
+                      target="_blank"
+                      className="pl-2"
+                    >
+                      {educationMaterial.file.fileName}
+                    </a>
+                  </Form.Text>
+                </div>
+            }
+          </Col>
+        </Row>
+      </Form>
+    </Dialog>
+  );
+};
+
+ViewEducationMaterial.propTypes = {
+  showView: PropTypes.bool,
+  handleViewClose: PropTypes.func,
+  educationMaterial: PropTypes.array
+};
+
+export default withLocalize(ViewEducationMaterial);
