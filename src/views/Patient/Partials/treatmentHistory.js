b941ce63653e754d588379ac9398794d8a86ca5b
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2021-04-08 11:46:31 +0700

    [TASK] Improve patient detail page
    refs TRA-423

--- src/views/Patient/Partials/treatmentHistory.js
+++ src/views/Patient/Partials/treatmentHistory.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { getTranslate } from 'react-localize-redux';
 import CustomTable from 'components/Table';
-import { EditAction, ViewAction } from 'components/ActionIcons';
 import { getTreatmentPlans } from 'store/treatmentPlan/actions';
 import * as ROUTES from 'variables/routes';
 import CreateButton from 'views/Patient/Partials/createButton';
 import { renderStatusBadge } from 'utils/treatmentPlan';
+import { Button } from 'react-bootstrap/esm/index';
 
 let timer = null;
 const TreatmentHistory = () => {
@@ -22,8 +22,7 @@ const TreatmentHistory = () => {
     { name: 'name', title: translate('common.treatment_name') },
     { name: 'treatment_status', title: translate('common.status') },
     { name: 'start_date', title: translate('common.start_date') },
-    { name: 'end_date', title: translate('common.end_date') },
-    { name: 'action', title: translate('common.action') }
+    { name: 'end_date', title: translate('common.end_date') }
   ];
   const [pageSize, setPageSize] = useState(10);
   const [currentPage, setCurrentPage] = useState(0);
@@ -58,49 +57,51 @@ const TreatmentHistory = () => {
     }
   }, [currentPage, pageSize, searchValue, filters, patientId, dispatch]);
 
-  const handleEdit = (id) => {
-    history.push(ROUTES.TREATMENT_PLAN_EDIT.replace(':patientId', patientId).replace(':id', id));
-  };
-
-  const handleView = (id) => {
-    history.push(ROUTES.VIEW_TREATMENT_PLAN_DETAIL.replace(':patientId', patientId).replace(':id', id));
+  const handleRowClick = (row) => {
+    history.push(ROUTES.VIEW_TREATMENT_PLAN_DETAIL.replace(':patientId', patientId).replace(':id', row.id));
   };
 
   return (
-    <div className="mt-3">
-      <h5>
-        {translate('treatment_plan.treatment_history')}
-      </h5>
-      <CustomTable
-        pageSize={pageSize}
-        setPageSize={setPageSize}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        totalCount={totalCount}
-        setSearchValue={setSearchValue}
-        setFilters={setFilters}
-        filters={filters}
-        columns={columns}
-        columnExtensions={columnExtensions}
-        rightButton={<CreateButton />}
-        rows={treatmentPlans.map(treatmentPlan => {
-          const action = (
-            <>
-              <ViewAction onClick={() => handleView(treatmentPlan.id)} />
-              <EditAction className="ml-1" onClick={() => handleEdit(treatmentPlan.id)} />
-            </>
-          );
-
-          return {
-            name: treatmentPlan.name,
-            treatment_status: renderStatusBadge(treatmentPlan),
-            start_date: treatmentPlan.start_date,
-            end_date: treatmentPlan.end_date,
-            action
-          };
-        })}
-      />
-    </div>
+    <>
+      <div className="mt-3 d-flex">
+        <h5>
+          {translate('treatment_plan.treatment_history')}
+        </h5>
+        <Button
+          className="ml-auto"
+          variant="outline-primary"
+          as={Link}
+          to={ROUTES.PATIENT}
+        >
+          &lt; {translate('patient.back_to_list')}
+        </Button>
+      </div>
+      <div className="mt-3">
+        <CustomTable
+          pageSize={pageSize}
+          setPageSize={setPageSize}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalCount={totalCount}
+          setSearchValue={setSearchValue}
+          setFilters={setFilters}
+          filters={filters}
+          columns={columns}
+          columnExtensions={columnExtensions}
+          onRowClick={handleRowClick}
+          rightButton={<CreateButton />}
+          rows={treatmentPlans.map(treatmentPlan => {
+            return {
+              id: treatmentPlan.id,
+              name: treatmentPlan.name,
+              treatment_status: renderStatusBadge(treatmentPlan),
+              start_date: treatmentPlan.start_date,
+              end_date: treatmentPlan.end_date
+            };
+          })}
+        />
+      </div>
+    </>
   );
 };
 
