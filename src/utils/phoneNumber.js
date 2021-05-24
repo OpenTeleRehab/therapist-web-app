4ed68e7a126b87e1f10e7398affdca78df5ecdcb
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2021-05-24 10:29:55 +0700

    [IMP] Improve phone format
    refs TRA-460

--- src/utils/phoneNumber.js
+++ src/utils/phoneNumber.js
@@ -1,7 +1,7 @@
 const CustomPhoneNumber = (dialCode, phoneNumber) => {
   const phoneNumberOnly = phoneNumber.split(dialCode);
 
-  return dialCode ? `(+ ${dialCode})-` + phoneNumberOnly[1] : phoneNumber;
+  return dialCode ? `(+${dialCode})-` + phoneNumberOnly[1] : phoneNumber;
 };
 
 export default CustomPhoneNumber;
