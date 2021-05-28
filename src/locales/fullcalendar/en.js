3ae8ec9812aa52cb9ed3ce7498fc50e7596dc825
Chivy Lim <chivy@web-essentials.asia>
2021-05-28 13:15:52 +0700

    [IMP] Update calendar button style
    refs TRA-443

---
+++ src/locales/fullcalendar/en.js
@@ -0,0 +1,28 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var en = {
+  code: 'en',
+  week: {
+    dow: 0,
+    doy: 4
+  },
+  direction: 'ltr',
+  buttonText: {
+    prev: 'Prev',
+    next: 'Next',
+    prevYear: 'Prev year',
+    nextYear: 'Next year',
+    year: 'Year',
+    today: 'Today',
+    month: 'Month',
+    week: 'Week',
+    day: 'Day',
+    list: 'List'
+  },
+  weekText: 'W',
+  allDayText: 'All-day',
+  moreLinkText: 'More',
+  noEventsText: 'No events to display'
+};
+
+exports.default = en;
