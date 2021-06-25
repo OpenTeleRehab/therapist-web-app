ff805299046841d198bee07b48f37dcf9e6b65c6
Sophea Phy <sophea@web-essentials.co>
2021-06-25 10:59:08 +0700

    [IMP] Make call/chat button in the patient info
    refs TRA-645

--- src/views/Patient/Partials/patientInfo.js
+++ src/views/Patient/Partials/patientInfo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import * as ROUTES from 'variables/routes';
@@ -22,12 +22,25 @@ import AgeCalculation from 'utils/age';
 import { activateDeactivateAccount, getUsers, deleteAccount } from 'store/user/actions';
 import CreatePatient from 'views/Patient/create';
 import Dialog from 'components/Dialog';
+import {
+  getChatRooms, selectRoom, setIsOnChatPage
+} from '../../../store/rocketchat/actions';
+import { loadMessagesInRoom, sendNewMessage } from '../../../utils/rocketchat';
+import { markMessagesAsRead } from '../../../utils/chat';
+import RocketchatContext from '../../../context/RocketchatContext';
+import {
+  showErrorNotification
+} from '../../../store/notification/actions';
+import { generateHash } from '../../../utils/general';
+import { CALL_STATUS } from '../../../variables/rocketchat';
 
 const PatientInfo = ({ id, translate }) => {
   const dispatch = useDispatch();
   const users = useSelector(state => state.user.users);
   const { profile } = useSelector((state) => state.auth);
+  const therapist = useSelector(state => state.auth.profile);
   const countries = useSelector(state => state.country.countries);
+  const { authToken, chatRooms } = useSelector(state => state.rocketchat);
   const [editId, setEditId] = useState('');
   const [show, setShow] = useState(false);
   const [showActivateDeactivateDialog, setShowActivateDeactivateDialog] = useState(false);
@@ -35,6 +48,7 @@ const PatientInfo = ({ id, translate }) => {
   const [disabledConfirmButton, setDisabledConfirmButton] = useState(false);
   const [isSecondaryTherapist, setIsSecondaryTherapist] = useState(false);
   const history = useHistory();
+  const chatSocket = useContext(RocketchatContext);
 
   const [formFields, setFormFields] = useState({
     name: '',
@@ -63,11 +77,19 @@ const PatientInfo = ({ id, translate }) => {
         country: getCountryName(data.country_id, countries),
         note: data.note || '',
         age: AgeCalculation(data.date_of_birth, translate) || '',
+        chat_user_id: data.chat_user_id || '',
         enabled: data.enabled
       });
       setIsSecondaryTherapist(data.secondary_therapists.includes(profile.id));
     }
   }, [id, users, countries, translate, profile]);
+
+  useEffect(() => {
+    if (therapist && therapist.chat_user_id && therapist.chat_rooms.length && authToken) {
+      dispatch(getChatRooms());
+    }
+    dispatch(setIsOnChatPage(false));
+  }, [authToken, dispatch, therapist]);
 
   const handleEdit = (id) => {
     setEditId(id);
@@ -116,13 +138,55 @@ const PatientInfo = ({ id, translate }) => {
     });
   };
 
+  const handleSelectRoomToChat = () => {
+    const findIndex = chatRooms.findIndex(r => r.u._id === formFields.chat_user_id);
+    if (findIndex !== -1) {
+      dispatch(selectRoom(chatRooms[findIndex]));
+      loadMessagesInRoom(chatSocket, chatRooms[findIndex].rid, therapist.id);
+      setTimeout(() => {
+        markMessagesAsRead(chatSocket, chatRooms[findIndex].rid, therapist.id);
+      }, 1000);
+      history.push(ROUTES.CHAT_OR_CALL);
+    } else {
+      dispatch(showErrorNotification('chat_or_call', 'error_message.chat_nonactivated'));
+    }
+  };
+
+  const handleSelectRoomToCall = () => {
+    const findIndex = chatRooms.findIndex(r => r.u._id === formFields.chat_user_id);
+    if (findIndex !== -1) {
+      const newMessage = {
+        _id: generateHash(),
+        rid: chatRooms[findIndex].rid,
+        msg: CALL_STATUS.AUDIO_STARTED
+      };
+      sendNewMessage(chatSocket, newMessage, therapist.id);
+      dispatch(selectRoom(chatRooms[findIndex]));
+      loadMessagesInRoom(chatSocket, chatRooms[findIndex].rid, therapist.id);
+      setTimeout(() => {
+        markMessagesAsRead(chatSocket, chatRooms[findIndex].rid, therapist.id);
+      }, 1000);
+      history.push(ROUTES.CHAT_OR_CALL);
+    } else {
+      dispatch(showErrorNotification('chat_or_call', 'error_message.chat_nonactivated'));
+    }
+  };
+
   return (
     <>
       <div className="btn-toolbar mb-2 mb-md-0 d-flex float-right">
-        <Button variant="link" className="mr-2 btn-circle-lg btn-light-blue">
+        <Button
+          variant="link"
+          className="mr-2 btn-circle-lg btn-light-blue"
+          onClick={() => handleSelectRoomToCall()}
+        >
           <FaPhoneAlt size={20} />
         </Button>
-        <Button variant="link" className="mr-4 btn-circle-lg btn-light-blue">
+        <Button
+          variant="link"
+          className="mr-4 btn-circle-lg btn-light-blue"
+          onClick={() => handleSelectRoomToChat()}
+        >
           <BsFillChatSquareFill size={20} />
         </Button>
         {!isSecondaryTherapist &&
