5ab2236b4ad8f0ad1547594f99018ddafe621f8f
Sophea Phy <sophea@web-essentials.co>
2022-01-21 17:06:58 +0700

    [BUGFIX] Truncate long library category text
    refs TRA-731

--- src/views/TreatmentPlan/Activity/Questionnaire/index.js
+++ src/views/TreatmentPlan/Activity/Questionnaire/index.js
@@ -209,7 +209,7 @@ const Questionnaire = ({ translate, selectedQuestionnaires, onSectionChange, vie
                   questionnaireCategoryTreeData.map(category => (
                     <Card key={category.value} className="mb-3 rounded">
                       <Accordion.Toggle eventKey={category.value} className="d-flex align-items-center card-header border-0">
-                        {category.label}
+                        <span className="text-truncate pr-2">{category.label}</span>
                         <div className="ml-auto text-nowrap">
                           <span className="mr-3">
                             {selectedCategories[category.value] ? selectedCategories[category.value].length : 0}
