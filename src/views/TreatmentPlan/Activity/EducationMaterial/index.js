1bda44db74fa60048645c81e7c6e8bd8b8688e8f
Chivy Lim <chivy@web-essentials.co>
2023-01-24 09:27:14 +0700

    [BUGFIX] Fix error when category has no children
    refs TRA-407 TRA-852

--- src/views/TreatmentPlan/Activity/EducationMaterial/index.js
+++ src/views/TreatmentPlan/Activity/EducationMaterial/index.js
@@ -208,7 +208,7 @@ const EducationMaterial = ({ translate, selectedMaterials, onSectionChange, view
                       <Accordion.Collapse eventKey={category.value}>
                         <Card.Body>
                           <CheckboxTree
-                            nodes={category.children}
+                            nodes={category.children || []}
                             checked={selectedCategories[category.value] ? selectedCategories[category.value] : []}
                             expanded={expanded}
                             onCheck={checked => handleSetSelectedCategories(category.value, checked)}
