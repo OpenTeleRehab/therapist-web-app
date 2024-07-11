b72ff7c4929dc85b97b850b3d47332cc3a8f5b53
Sophea Phy <sophea@web-essentials.co>
2024-07-11 09:28:28 +0700

    [TASK] Display patient and transfer list with tab
    refs TRA-912

--- src/views/Patient/index.js
+++ src/views/Patient/index.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Badge, Button } from 'react-bootstrap';
-import { Link, useHistory } from 'react-router-dom';
+import { Badge, Button, Nav, Tab } from 'react-bootstrap';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { TiWarning } from 'react-icons/all';
 import { BsPlus, BsChatDotsFill } from 'react-icons/bs';
-import { BiTransfer } from 'react-icons/bi';
 import { ProgressBar } from 'react-step-progress-bar';
 import PropTypes from 'prop-types';
 import moment from 'moment/moment';
@@ -27,9 +26,16 @@ import { loadMessagesInRoom } from '../../utils/rocketchat';
 import { showErrorNotification } from '../../store/notification/actions';
 import RocketchatContext from '../../context/RocketchatContext';
 import { getTransfers } from '../../store/transfer/actions';
+import queryString from 'query-string';
+import Transfer from './Transfer';
+
+const VIEW_PATIENT = 'patient';
+const VIEW_TRANSFER = 'transfer';
 
 const Patient = () => {
   const dispatch = useDispatch();
+  const { search } = useLocation();
+  const [view, setView] = useState(VIEW_PATIENT);
   const [show, setShow] = useState(false);
   const [editId, setEditId] = useState('');
   const { users, loading } = useSelector(state => state.user);
@@ -109,6 +115,14 @@ const Patient = () => {
     dispatch(getTransfers());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (queryString.parse(search).tab === VIEW_TRANSFER) {
+      setView(VIEW_TRANSFER);
+    } else {
+      setView(VIEW_PATIENT);
+    }
+  }, [search]);
+
   const getSecondaryTherapist = (user) => {
     let primaryTherapistHTML = '';
     let secondaryTherapistHTML = '';
@@ -168,78 +182,95 @@ const Patient = () => {
 
   return (
     <>
-      <div className="d-flex justify-content-between align-items-center mb-3">
-        <h1 className="mb-0">{translate('patient.list')}</h1>
-        <div className="btn-toolbar gap-3">
-          <Link className="btn btn-outline-primary" to={ROUTES.PATIENT_TRANSFER_LIST}>
-            <BiTransfer className="mr-1" /> {translate('transfer.list')}
-            {pendingTransfers.length > 0 && <Badge className="ml-1" variant="danger">{ pendingTransfers.length }</Badge>}
-          </Link>
-          <Button className="ml-3" variant="primary" onClick={handleShow}>
-            <BsPlus className="mr-1"/>
-            {translate('patient.new')}
-          </Button>
-        </div>
-        {show && <CreatePatient handleClose={handleClose} show={show} editId={editId}/>}
-      </div>
-      <CustomTable
-        loading={loading}
-        pageSize={pageSize}
-        setPageSize={setPageSize}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        setSearchValue={setSearchValue}
-        setFilters={setFilters}
-        filters={filters}
-        columns={columns}
-        columnExtensions={columnExtensions}
-        defaultHiddenColumnNames={defaultHiddenColumnNames}
-        onRowClick={handleRowClick}
-        hover="hover-primary"
-        rows={users.map(user => {
-          const transfer = transfers.find(item => item.patient_id === user.id && item.therapist_type === 'lead');
-          const room = chatRooms.find(r => r.rid.includes(user.chat_user_id));
-          const unread = room ? room.unread : 0;
+      <Tab.Container mountOnEnter activeKey={view}>
+        <Nav variant="tabs" className="mb-3">
+          <Nav.Item>
+            <Nav.Link as={Link} to={ROUTES.PATIENT} eventKey={VIEW_PATIENT}>
+              {translate('patient.list')}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to={ROUTES.PATIENT_TRANSFER} eventKey={VIEW_TRANSFER}>
+              {translate('transfer.list')} <Badge className="ml-1" variant="danger">{ pendingTransfers.length }</Badge>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
 
-          const notification = (
-            <div className="notify-lists d-flex align-items-center">
-              <div className="notify-list-item mr-2">
-                <ProgressBar width={75} percent={user.completed_percent || 0} />
+        <Tab.Content>
+          <Tab.Pane eventKey={VIEW_PATIENT}>
+            <div className="d-flex justify-content-end align-items-center mb-3">
+              <div className="btn-toolbar gap-3">
+                <Button className="ml-3" variant="primary" onClick={handleShow}>
+                  <BsPlus className="mr-1"/>
+                  {translate('patient.new')}
+                </Button>
               </div>
-              {user.total_pain_threshold > 0 && (
-                <div className="notify-list-item mr-2">
-                  <TiWarning className="threshold-icon mr-1" size={20} color={scssColors.orange}/>
-                  <sup>{user.total_pain_threshold}</sup>
-                </div>
-              )}
-              {unread > 0 && (
-                <div className="notify-list-item">
-                  <Button className="text-decoration-none" onClick={(event) => handleSelectRoomToCall(event, user)} variant="link">
-                    <BsChatDotsFill className="chat-icon mr-1" size={20} color={scssColors.primary} />
-                    <sup>{unread > 99 ? '99+' : unread}</sup>
-                  </Button>
-                </div>
-              )}
+              {show && <CreatePatient handleClose={handleClose} show={show} editId={editId}/>}
             </div>
-          );
+            <CustomTable
+              loading={loading}
+              pageSize={pageSize}
+              setPageSize={setPageSize}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              setSearchValue={setSearchValue}
+              setFilters={setFilters}
+              filters={filters}
+              columns={columns}
+              columnExtensions={columnExtensions}
+              defaultHiddenColumnNames={defaultHiddenColumnNames}
+              onRowClick={handleRowClick}
+              hover="hover-primary"
+              rows={users.map(user => {
+                const transfer = transfers.find(item => item.patient_id === user.id && item.therapist_type === 'lead');
+                const room = chatRooms.find(r => r.rid.includes(user.chat_user_id));
+                const unread = room ? room.unread : 0;
 
-          return {
-            id: user.id,
-            identity: user.identity,
-            last_name: user.last_name,
-            first_name: user.first_name,
-            email: user.email,
-            date_of_birth: user.date_of_birth !== null ? moment(user.date_of_birth, 'YYYY-MM-DD').locale('en').format(settings.date_format) : '',
-            age: user.date_of_birth !== null ? AgeCalculation(user.date_of_birth, translate) : '',
-            ongoing_treatment_plan: user.ongoingTreatmentPlan.length ? user.ongoingTreatmentPlan[0].name : user.upcomingTreatmentPlan ? user.upcomingTreatmentPlan.name : '',
-            treatment_status: renderStatusBadge(user.ongoingTreatmentPlan.length ? user.ongoingTreatmentPlan[0] : user.lastTreatmentPlan),
-            next_appointment: user.upcoming_appointment ? moment.utc(user.upcoming_appointment.start_date).local().format(settings.date_format + ' hh:mm A') : '',
-            secondary_therapist: <span dangerouslySetInnerHTML={{ __html: getSecondaryTherapist(user) }}></span>,
-            notification,
-            transfer: transfer && transfer.from_therapist_id === therapist.id && <Badge pill variant="warning">{translate(`transfer.status.${transfer.status}`)}</Badge>
-          };
-        })}
-      />
+                const notification = (
+                  <div className="notify-lists d-flex align-items-center">
+                    <div className="notify-list-item mr-2">
+                      <ProgressBar width={75} percent={user.completed_percent || 0} />
+                    </div>
+                    {user.total_pain_threshold > 0 && (
+                      <div className="notify-list-item mr-2">
+                        <TiWarning className="threshold-icon mr-1" size={20} color={scssColors.orange}/>
+                        <sup>{user.total_pain_threshold}</sup>
+                      </div>
+                    )}
+                    {unread > 0 && (
+                      <div className="notify-list-item">
+                        <Button className="text-decoration-none" onClick={(event) => handleSelectRoomToCall(event, user)} variant="link">
+                          <BsChatDotsFill className="chat-icon mr-1" size={20} color={scssColors.primary} />
+                          <sup>{unread > 99 ? '99+' : unread}</sup>
+                        </Button>
+                      </div>
+                    )}
+                  </div>
+                );
+
+                return {
+                  id: user.id,
+                  identity: user.identity,
+                  last_name: user.last_name,
+                  first_name: user.first_name,
+                  email: user.email,
+                  date_of_birth: user.date_of_birth !== null ? moment(user.date_of_birth, 'YYYY-MM-DD').locale('en').format(settings.date_format) : '',
+                  age: user.date_of_birth !== null ? AgeCalculation(user.date_of_birth, translate) : '',
+                  ongoing_treatment_plan: user.ongoingTreatmentPlan.length ? user.ongoingTreatmentPlan[0].name : user.upcomingTreatmentPlan ? user.upcomingTreatmentPlan.name : '',
+                  treatment_status: renderStatusBadge(user.ongoingTreatmentPlan.length ? user.ongoingTreatmentPlan[0] : user.lastTreatmentPlan),
+                  next_appointment: user.upcoming_appointment ? moment.utc(user.upcoming_appointment.start_date).local().format(settings.date_format + ' hh:mm A') : '',
+                  secondary_therapist: <span dangerouslySetInnerHTML={{ __html: getSecondaryTherapist(user) }}></span>,
+                  notification,
+                  transfer: transfer && transfer.from_therapist_id === therapist.id && <Badge pill variant="warning">{translate(`transfer.status.${transfer.status}`)}</Badge>
+                };
+              })}
+            />
+          </Tab.Pane>
+          <Tab.Pane eventKey={VIEW_TRANSFER}>
+            <Transfer/>
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
       { !_.isEmpty(colorScheme) && customColorScheme(colorScheme) }
     </>
   );
