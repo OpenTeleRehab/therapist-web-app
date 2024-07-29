a4fcbde1320d6701fc50ec2a57d6fc0a533d500e
Chanthorn Ngeth <chanthorn@web-essentials.co>
2024-07-29 17:52:42 +0700

    [IMP] Change icons to button
    refs TRA-903

--- src/components/ActionIcons/index.js
+++ src/components/ActionIcons/index.js
@@ -1,7 +1,5 @@
 export * from './EditAction';
 export * from './DeleteAction';
 export * from './ViewAction';
-export * from './AcceptAction';
 export * from './FavoriteAction';
 export * from './CopyAction';
-export * from './RejectAction';
