7737f7d094b6e98f6cd2ecf4893ba5f746258787
Phearoth Maksay <phearoth@web-essentials.co>
2025-04-04 15:34:15 +0700

    [IMP] Apply color theme to new menu items
    refs TRA-1040

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import SupersetDashboard from 'components/SupersetDashboard';
+import { useSelector } from 'react-redux';
+import _ from 'lodash';
+import customColorScheme from '../../utils/customColorScheme';
 
 const Dashboard = () => {
+  const { colorScheme } = useSelector(state => state.colorScheme);
+
   return (
     <>
       <SupersetDashboard dashboardId={process.env.REACT_APP_SUPERSET_DASHBOARD_ID_FOR_THERAPIST} />
+      { !_.isEmpty(colorScheme) && customColorScheme(colorScheme) }
     </>
   );
 };
